b68aa2c744d42f91c5573b25cd083e4b1a02ede9
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-08-02 21:19:25 +0300

    testing

--- TestTask/TestTask.cpp
+++ TestTask/TestTask.cpp
@@ -15,6 +15,35 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+void draw_heptagon() 
+{
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.6f, 0.8f, -5.0f);
+	glVertex3f(0.0f, 1.0f, -5.0f);
+	glVertex3f(0.6f, 0.8f, -5.0f);
+	glEnd();
+	glBegin(GL_QUADS);
+	glVertex3f(0.6f, 0.8f, -5.0f);
+	glVertex3f(0.8f, 0.4f, -5.0f);
+	glVertex3f(-0.8f, 0.4f, -5.0f);
+	glVertex3f(-0.6f, 0.8f, -5.0f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex3f(0.8f, 0.4f, -5.0f);
+	glVertex3f(0.6f, 0.0f, -5.0f);
+	glVertex3f(-0.6f, 0.0f, -5.0f);
+	glVertex3f(-0.8f, 0.4f, -5.0f);
+	glEnd();
+}
+
+//Initializes 3D rendering
+void initializeRendering()
+{
+	glfwInit();
+	//Makes 3D drawing work when something is in front of something else
+	glEnable(GL_DEPTH_TEST);
+}
 
 int main()
 {
@@ -25,6 +54,7 @@ int main()
 		return 1;
 	}
 	else cout << "Hello GLFW!" << endl;
+	glEnable(GL_DEPTH_TEST);
 
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 	if (!window)
@@ -34,11 +64,56 @@ int main()
 		return 1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+	glfwSwapInterval(20);
+	glfwSetKeyCallback(window, key_callback);
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSetKeyCallback(window, key_callback);
-		glClear(GL_COLOR_BUFFER_BIT);
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		//
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //clear background screen to black
+
+		//Clear information from last draw
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+		glLoadIdentity(); //Reset the drawing perspective
+
+		glBegin(GL_QUADS); //Begin quadrilateral coordinates
+
+		//Trapezoid
+		glVertex3f(-0.7f, -1.5f, -5.0f);
+		glVertex3f(0.7f, -1.5f, -5.0f);
+		glVertex3f(0.4f, -0.5f, -5.0f);
+		glVertex3f(-0.4f, -0.5f, -5.0f);
+
+		glEnd(); //End quadrilateral coordinates
+
+		glBegin(GL_TRIANGLES); //Begin triangle coordinates
+
+		//Pentagon
+		glVertex3f(0.5f, 0.5f, -5.0f);
+		glVertex3f(1.5f, 0.5f, -5.0f);
+		glVertex3f(0.5f, 1.0f, -5.0f);
+
+		glVertex3f(0.5f, 1.0f, -5.0f);
+		glVertex3f(1.5f, 0.5f, -5.0f);
+		glVertex3f(1.5f, 1.0f, -5.0f);
+
+		glVertex3f(0.5f, 1.0f, -5.0f);
+		glVertex3f(1.5f, 1.0f, -5.0f);
+		glVertex3f(1.0f, 1.5f, -5.0f);
+
+		//Triangle
+		glVertex3f(-0.5f, 0.5f, -5.0f);
+		glVertex3f(-1.0f, 1.5f, -5.0f);
+		glVertex3f(-1.5f, 0.5f, -5.0f);
+
+		glEnd(); //End triangle coordinates
+		//
+
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
