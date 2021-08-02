eb3fe2c0e2030ac8c785f12498877a9d18b880b4
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-08-02 20:53:06 +0300

    added cmake commands for gl

--- TestTask/TestTask.cpp
+++ TestTask/TestTask.cpp
@@ -1,10 +1,20 @@
-﻿#define GLFW_INCLUDE_NONE
+﻿#ifdef _WIN32
+	#include <Windows.h>
+#endif 
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <GL/gl.h>
 #include <iostream>
 
 #include "TestTask.h"
 
 using namespace std;
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main()
 {
@@ -23,7 +33,16 @@ int main()
 		cout << "Window or OpenGL context creation failed!" << endl;
 		return 1;
 	}
-	while (true)
-	{}
-	return 0;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSetKeyCallback(window, key_callback);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
--- TestTask/TestTask.h
+++ TestTask/TestTask.h
@@ -1,18 +1,19 @@
 ﻿#pragma once
 
-class IShape
+struct IShape
 {
-	public: virtual bool fillColor(float RED, float GREEN, float BLUE) = 0;
-	public:	virtual void setPosition(float X, float Y) const = 0;
-	public:	virtual void rotateShape(float angle) = 0;
+	virtual bool fillColor(float RED, float GREEN, float BLUE) = 0;
+	virtual void setPosition(float X, float Y) const = 0;
+	virtual void rotateShape(float angle) = 0;
+	virtual void setScale(double scale) = 0;
 };
 
-class IMovable
+struct IMovable
 {
-
+	virtual void setSpeed(double rotateSpeed) = 0;
 };
 
-class Dot : public IShape
+class Dot : public IShape, public IMovable
 {
 
 };
