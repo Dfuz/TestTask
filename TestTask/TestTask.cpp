d2da8d86d02e7f92c2277a08bdd83cb252fdb989
Vadim Perfilyev <vadimperfilev@gmail.com>
2021-08-02 19:08:58 +0300

    Add project files.

---
+++ TestTask/TestTask.cpp
@@ -0,0 +1,12 @@
+﻿// TestTask.cpp : Defines the entry point for the application.
+//
+
+#include "TestTask.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
